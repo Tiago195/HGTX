7af8e51f9933eb69d616daebd36f901959dd6128
Tiago195 <tiagocf475@gmail.com>
2022-08-20 20:48:11 -0300

    :sparkles: page de consultar usuarios
    page com css quase okay, esta faltando media query, e tbm esta faltando algumas funcionalidades desta pagina

---
+++ front-end/src/App.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import style from './app.module.css';
+import { FiSearch, FiRefreshCw, FiMoreVertical } from 'react-icons/fi';
+import axios from 'axios';
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/users', {
+      headers: { Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjo0LCJuYW1lIjoiQWRtaW4iLCJlbWFpbCI6IkFkbWluQEFkbWluLmNvbSIsInN0YXR1cyI6IkF0aXZvIn0sImlhdCI6MTY2MDk1ODQ0NCwiZXhwIjoxNjYxNTYzMjQ0fQ.xidPDGuB4Ih630iAmtdV8HyHmbcpk3-h_2n_cqPqjIQ' }
+    })
+      .then(response => setUsers(response.data));
+  }, []);
+
+  const cpfFormater = (cpf) => {
+    return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, (r, a1, a2, a3, a4) => `${a1}.${a2}.${a3}-${a4}`);
+  };
+
+
+  return (
+    <div className={style.root_container}>
+      <div className={style.header}>
+        <h1>Consultar usuários</h1>
+      </div>
+      <div className={style.main}>
+        <button className={style.add}>+ Novo</button>
+        <div className={style.search_container}>
+          <div className={style.search}>
+            <label htmlFor="search">
+              <FiSearch />
+              <input type="text" name="search" id="search" placeholder='Pesquisar' />
+            </label>
+            <button className={style.refresh}><FiRefreshCw /></button>
+          </div>
+          <div className={style.user_container}>
+            <table className={style.table}>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Nome</th>
+                  <th>E-mail</th>
+                  <th>CPF</th>
+                  <th>Ações</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map(user => (
+                  <tr key={user.id}>
+                    <td>{user.id.toString().padStart(4, '0')}</td>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>{cpfFormater(user.cpf)}</td>
+                    <td><button><FiMoreVertical /></button></td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div >
+  );
+}
+
+export default App;
